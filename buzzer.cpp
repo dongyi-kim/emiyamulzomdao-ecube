dde164d3c3747a4730b708157f4463043ce6b172
hyeonjae0310 <hyeonjae8830@gmail.com>
2016-06-14 12:22:57 +0900

    Update buzzer.cpp

--- buzzer.cpp
+++ buzzer.cpp
@@ -1,3 +1,8 @@
+/*
+	@file	-	buzzer.cpp
+	@author	-	
+	@brief	-	control buzzer
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,33 +17,39 @@
 #include "dipsw.h"
 #include "thread_manager.h"
 
-#define DRIVER_BUZZER			"/dev/cnbuzzer"
-#define MAX_BUZZER_NUMBER		36
+#define DRIVER_BUZZER			"/dev/cnbuzzer" ///< buzzer file driver
+#define MAX_BUZZER_NUMBER		36 ///< maximum number of sound type of buzzer.
 
+//do(1,13,25,37) ,do#(2,14,26)
+//re(3,15,27), re#(4,16,28)
+//mi(5,17,29)
+//fa(6,18,30), fa#(7,19,31)
+//sol(8,20,32), sol#(9,21,33)
+//ra(10,22,34), ra#(11,23,35)
+//si(12,24,36)  
+//control buzzer sound, alarm for 100000 micro second.
 void buzzer(int buzzerNumber)
 {
 	int fd;
 
-	// open  driver 
-	fd = open(DRIVER_BUZZER, O_RDWR);
+	fd = open(DRIVER_BUZZER, O_RDWR);///< open driver
 	if ( fd < 0 )
 	{
 		perror("driver (//dev//cnbuzzer) open error.\n");
 		return;
 	}
-	// control led
-	pthread_mutex_lock(thread_manager::get_buzz());
+	pthread_mutex_lock(thread_manager::get_buzz());///< waiting for priting picture of oled.
 	write(fd, &buzzerNumber, 4);
 	pthread_mutex_unlock(thread_manager::get_buzz());
 
-	usleep(100000);
-    buzzerNumber = 0;
+	usleep(100000);///< waiting 100000 micro second.
+    	buzzerNumber = 0;
 
-	pthread_mutex_lock(thread_manager::get_buzz());
+	pthread_mutex_lock(thread_manager::get_buzz());///< waiting for priting picture of oled.
 	write(fd, &buzzerNumber, 4);
 	pthread_mutex_unlock(thread_manager::get_buzz());
 
-    close(fd);
+    	close(fd);///< close file drvier.
 	
 	return;
 }
@@ -47,11 +58,12 @@ void dip_buzzer(const int arr[], const int arr_len)
 {
 	int i;
 
-	int dips = dipsw();
+	int dips = dipsw();///< load dip switch. 
 
+	//if first dip switch turn on, buzzer sound off.
 	if((dips & 1) != 1)
 	{
-
+		// if first dip switch turn off, sequentially alarm the buzzer.
 		for(i = 0 ; i < arr_len ; i++)
 		{
 			buzzer(arr[i]);
