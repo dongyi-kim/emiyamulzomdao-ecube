e665cfc8f26cee3c9932da23d850f14db00ae5fc
hyeonjae0310 <hyeonjae8830@gmail.com>
2016-06-14 12:58:23 +0900

    Update fnd.cpp

--- fnd.cpp
+++ fnd.cpp
@@ -1,3 +1,10 @@
+/*
+        @file   -       fnd.cpp
+        @author -       
+        @brief  -       control 7-segment
+        @reference -    system programming([Practice]7th_Device driver.pptx)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,25 +20,25 @@
 #include "common.h"
 #include "thread_manager.h"
 
-#define DRIVER_SEGMENT		"/dev/cnfnd"
+#define DRIVER_SEGMENT		"/dev/cnfnd"///< 7-segment file driver path
 
-#define MAX_FND_NUM		6
+#define MAX_FND_NUM		6///< the maximum number of 7-segment.
 
 #define  DOT_OR_DATA	0x80
 
 const unsigned short segNum[11] =
         {
-                0x3F, // 0
-                0x06,
-                0x5B,
-                0x4F,
-                0x66,
-                0x6D,
-                0x7D,
-                0x27,
-                0x7F,
-                0x6F, // 9
-                0x00
+                0x3F, ///< 0
+                0x06, ///< 1
+                0x5B, ///< 2
+                0x4F, ///< 3
+                0x66, ///< 4
+                0x6D, ///< 5
+                0x7D, ///< 6
+                0x27, ///< 7
+                0x7F, ///< 8
+                0x6F, ///< 9
+                0x00 ///< off
         };
 const unsigned short segSelMask[MAX_FND_NUM] =
         {
@@ -46,7 +53,7 @@ static struct termios oldt, newt;
 
 void fnd_changemode(int dir)
 {
-    if( dir == 1)
+    if(dir == 1)
     {
         tcgetattr(STDIN_FILENO , &oldt);
         newt = oldt;
@@ -60,33 +67,34 @@ void fnd_changemode(int dir)
     }
 }
 
-#define ONE_SEG_DISPLAY_TIME_USEC	1000
-// return 1 => exit  , 0 => success
+#define ONE_SEG_DISPLAY_TIME_USEC	1000 ///< display time each 7-segment
+
+/**
+display control 7-segment
+*/
 int fndDisp(int driverfile, int* num , int dotflag)
 {
-
-    int cSelCounter,loopCounter;
-    int temp, i, totalCount;
+    int cSelCounter;///< each 7-segment index
+    int temp;///< temporary variable.
     unsigned short wdata;
     int dotEnable[MAX_FND_NUM];
-    int fndChar[MAX_FND_NUM] = {0, 0, 0, 0, 0, 0};
-
+    int fndChar[MAX_FND_NUM] = {0, 0, 0, 0, 0, 0};///< each 7-segment value.
+    
+    //dot express
     for (i = 0; i < MAX_FND_NUM ; i++ )
     {
         dotEnable[i] = dotflag & (0x1 << i);
     }
-
-    cout<<(*num)<<endl;
-    // if 6 fnd
+    //7-segment display
     while(1) {
-
+        //if segment data is -1, initialize
         if( (*num) == -1 ) {
             for(i = 5 ; i >= 0 ; i--)
             {
                 fndChar[i] = 10;
             }
         }
-
+        //else each digit push fndChar
         else {
             temp = (*num)%1000000;
 
@@ -97,37 +105,33 @@ int fndDisp(int driverfile, int* num , int dotflag)
                 temp /= 10;
             }
         }
-        int duration = 1;
-        while(duration--) {
-            cSelCounter = 0;
-            while (cSelCounter < MAX_FND_NUM) {
+        cSelCounter = 0;
+        //display 7-segment value
+        while (cSelCounter < MAX_FND_NUM) {
                 wdata = segNum[fndChar[cSelCounter]] | segSelMask[cSelCounter];
                 if (dotEnable[cSelCounter])
                     wdata |= DOT_OR_DATA;
-                pthread_mutex_lock(thread_manager::get_seg());
+                pthread_mutex_lock(thread_manager::get_seg());///< waiting for printing picture of oled.
                 write(driverfile, &wdata, 2);
                 pthread_mutex_unlock(thread_manager::get_seg());
                 cSelCounter++;
-
-
-                usleep(ONE_SEG_DISPLAY_TIME_USEC);
-
-            }
+                usleep(ONE_SEG_DISPLAY_TIME_USEC);///< display time
         }
         wdata = 0;
-        pthread_mutex_lock(thread_manager::get_seg());
+        pthread_mutex_lock(thread_manager::get_seg());///< waiting for printing picture of oled.
         write(driverfile, &wdata, 2);
         pthread_mutex_unlock(thread_manager::get_seg());
     }
 
     return 1;
 }
-
+/**
+call the 7-segment display function.
+*/
 void segment(Shared* shared)
 {
     int fd;
-
-    // open  driver
+    // open 7-segment file driver
     fd = open(DRIVER_SEGMENT, O_RDWR);
     if ( fd < 0 )
     {
@@ -140,7 +144,6 @@ void segment(Shared* shared)
 
     fnd_changemode(0);
     close(fd);
-
     return;
 }
 
